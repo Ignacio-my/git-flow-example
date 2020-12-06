6b44ce1ca91da50339b65d009494e0dcb8c3e0cc
ignacio <ignacio.miranda@alumnos.uv.cl>
2020-12-06 20:13:31 -0300

    actualizacion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {version} from "./../package.json"
 
 function App() {
   return (
@@ -7,16 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Supongamos que puedes tocar la ropa y cambiar de música
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Program version : {version}
+        </p>
+        <p>
+          For the University of Valparaíso
+        </p>
+        <span>New Version</span>
       </header>
     </div>
   );
