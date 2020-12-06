6270f7e9de351653d87f44109a1fc51dcbbdb6df
ignacio <ignacio.miranda@alumnos.uv.cl>
2020-12-06 20:37:46 -0300

    terminado

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          supongamos que te puedes tocar la ropa
         </p>
         <a
           className="App-link"
